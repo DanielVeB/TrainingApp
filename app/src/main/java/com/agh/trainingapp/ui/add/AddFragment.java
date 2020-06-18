96aa97d33d6b897b198e6f03929f0b1b871a9549
DanielVeB <danielkurosz@gmail.com>
2020-06-18 21:52:36 +0200

    update

--- app/src/main/java/com/agh/trainingapp/ui/add/AddFragment.java
+++ app/src/main/java/com/agh/trainingapp/ui/add/AddFragment.java
@@ -1,4 +1,4 @@
-package com.agh.trainingapp.ui.home;
+package com.agh.trainingapp.ui.add;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -6,20 +6,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.agh.trainingapp.R;
 import com.agh.trainingapp.database.async.InsertTrainingTask;
 import com.agh.trainingapp.database.entities.Training;
 
-public class HomeFragment extends Fragment {
+public class AddFragment extends Fragment {
 
     private EditText training;
     private EditText description;
@@ -28,7 +24,7 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_add, container, false);
 
         training = view.findViewById(R.id.training);
         description = view.findViewById(R.id.description);
--- app/src/main/java/com/agh/trainingapp/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/agh/trainingapp/ui/gallery/GalleryFragment.java
@@ -1,5 +1,7 @@
 package com.agh.trainingapp.ui.gallery;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -65,6 +67,11 @@ public class GalleryFragment extends Fragment implements DispTrainingAdapter.OnT
 //                .replace(R.id.fragmentContainer, descriptionFragment)
 //                .addToBackStack(null)
 //                .commit();
+
+        Intent i = new Intent(getActivity(), TrainingInfo.class);
+        startActivity(i);
+        ((Activity) getActivity()).overridePendingTransition(0, 0);
+
     }
 
     @Override
