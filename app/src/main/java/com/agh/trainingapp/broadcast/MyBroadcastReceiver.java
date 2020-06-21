e0e1dcdd046c8dcfc470a713610f469682130ea2
Yurati <lukasz.dudek97@gmail.com>
2020-06-21 16:40:26 +0200

    Broadcast change

--- app/src/main/java/com/agh/trainingapp/broadcast/MyBroadcastReceiver.java
+++ app/src/main/java/com/agh/trainingapp/broadcast/MyBroadcastReceiver.java
@@ -10,7 +10,7 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     private static final String TAG = "MyBroadcastReceiver";
     @Override
     public void onReceive(Context context, Intent intent) {
-        String log = "Battery low. Consider closing app.";
+        String log = "Airplane mode does not affect application!.";
         Log.d(TAG, log);
         Toast.makeText(context, log, Toast.LENGTH_LONG).show();
     }
